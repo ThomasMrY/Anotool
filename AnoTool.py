8886c8e203713969e16badffaab69167afa9a4cb
ThomasMrY <yt14212@stu.xjtu.edu.cn>
2019-09-24 12:42:18 +0800

    3rd commit

--- AnoTool.py
+++ AnoTool.py
@@ -26,14 +26,14 @@ class Ui_Form(object):
         self.add_o = False
         self.A_extra = {}
         self.O_extra = {}
-        f = open('Attribute', encoding='utf-8')
+        f = open('Attribute', encoding='utf-8-sig')
         self.A_dict = {}
         for line in f.readlines():
             temp = line[:-1].split('：')
             self.A_dict[temp[0]] = temp[1].split('，')
         print(self.A_dict)
         f.close()
-        f = open('Object', encoding='utf-8')
+        f = open('Object', encoding='utf-8-sig')
         self.O_dict = {}
         for line in f.readlines():
             temp = line[:-1].split('：')
@@ -71,6 +71,8 @@ class Ui_Form(object):
         self.A_label_list = []
         with open('QListWidgetQSS.qss', 'r') as f:  # 导入QListWidget的qss样式
             self.list_style = f.read()
+        font = QtGui.QFont()
+        font.setPointSize(10)
         self.left_widget = QListWidget(self.groupBox)  # 左侧选项列表
         self.left_widget.setGeometry(QtCore.QRect(40, 40, 181, 360))
         self.left_widget.setStyleSheet(self.list_style)
@@ -79,8 +81,11 @@ class Ui_Form(object):
 
         with open('oQListWigetQSS.qss', 'r') as f:  # 导入QListWidget的qss样式
             self.olist_style = f.read()
+
         self.groupBox_5 = QtWidgets.QGroupBox(Form)
         self.groupBox_5.setGeometry(QtCore.QRect(850, 10, 291, 361))
+        font = QtGui.QFont()
+        font.setPointSize(10)
         self.oleft_widget = QListWidget(self.groupBox_5)  # 左侧选项列表
         self.oleft_widget.setGeometry(QtCore.QRect(10, 40, 181, 300))
         self.oleft_widget.setStyleSheet(self.olist_style)
@@ -172,9 +177,9 @@ class Ui_Form(object):
         self.pushButton_4.setGeometry(QtCore.QRect(930, 380, 211, 41))
         self.pushButton_4.setObjectName("pushButton_4")
         self.pushButton_4.clicked.connect(self.btn4_back)
-        self.textBrowser = QtWidgets.QTextBrowser(Form)
-        self.textBrowser.setGeometry(QtCore.QRect(40, 610, 461, 91))
-        self.textBrowser.setObjectName("textBrowser")
+        self.QTextEdit = QtWidgets.QTextEdit(Form)
+        self.QTextEdit.setGeometry(QtCore.QRect(40, 610, 461, 91))
+        self.QTextEdit.setObjectName("QTextEdit")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -222,7 +227,12 @@ class Ui_Form(object):
             self.O_comboBox_list.append(locals()["ostack" + str(i)])
             locals()['ostack' + str(i)].currentRowChanged.connect(self.orow_change)
 
+
         _translate = QtCore.QCoreApplication.translate
+        for i, key in enumerate(self.A_dict.keys()):
+            self.comboBox_11.setItemText(i, _translate("Form", key))
+        for i, key in enumerate(self.O_dict.keys()):
+            self.comboBox_11.setItemText(i + len(self.A_dict.keys()), _translate("Form", key))
         Form.setWindowTitle(_translate("Form", "Anotation Tool"))
         self.groupBox.setTitle(_translate("Form", "属性"))
         self.groupBox_5.setTitle(_translate("Form", "对象"))
@@ -238,89 +248,88 @@ class Ui_Form(object):
         self.label_15.setText(_translate("Form", "值:"))
         self.checkBox.setText(_translate("Form", "更新到图表"))
         self.pushButton_4.setText(_translate("Form", "上一张图片"))
-
     def row_change(self):
         cv = self.left_widget.currentIndex().data()
         i = self.left_widget.currentIndex().row()
         j = self.A_comboBox_list[i].currentIndex().row()
         vv = self.A_comboBox_list[i].currentIndex().data()
         self.A_answer[cv] = vv
-        print(self.A_answer)
+        self.QTextEdit.setPlainText(str("对象:\n")+str(self.O_answer)+"\n"+str("属性:\n")+str(self.A_answer)+"\n")
     def orow_change(self):
         cv = self.oleft_widget.currentIndex().data()
         i = self.oleft_widget.currentIndex().row()
         j = self.O_comboBox_list[i].currentIndex().row()
         vv = self.O_comboBox_list[i].currentIndex().data()
         self.O_answer[cv] = vv
-        print(self.O_answer)
+        self.QTextEdit.setPlainText(str("对象:\n") + str(self.O_answer) + "\n" + str("属性:\n") + str(self.A_answer) + "\n")
     def btn_save(self):
-        if self.num > len(self.file_list):
-            print("Exit!!!")
         self.num += 1
         with open("count","w") as f:
             f.write(str(self.num))
+        text = self.QTextEdit.toPlainText()
+        O_an = eval(text.split('\n')[1])
+        A_an = eval(text.split('\n')[3])
+        if os.path.exists("./Labels/" + self.file_list[self.num - 1] + ".json"):
+            os.remove("./Labels/" + self.file_list[self.num - 1] + ".json")
+        with open("./Labels/" + self.file_list[self.num - 1] + ".json", mode="a") as f:
+            json.dump(A_an, f,ensure_ascii=False)
+            json.dump(O_an, f,ensure_ascii=False)
+        if self.num > len(self.file_list) - 1:
+            sys.exit(0)
         pixmap = QPixmap("./Images/"+self.file_list[self.num])
         self.label_16.setPixmap(pixmap)
         self.label_16.setScaledContents(True)
-        Attribute_ = {}
-        Object_ = {}
-        for i, key in enumerate(self.A_dict.keys()):
-            Val = self.A_comboBox_list[i].currentText()
-            if Val != "none":
-                Attribute_[key] = Val
-        for i,key in enumerate(self.O_dict.keys()):
-            Val = self.O_comboBox_list[i].currentText()
-            if Val != "none":
-                Object_[key] = Val
-        Attribute = dict(Attribute_,**self.A_extra)
-        Object = dict(Object_,**self.O_extra)
-        with open("./Labels/"+self.file_list[self.num-1]+".json",mode="a") as f:
-            json.dump(Attribute, f)
-            json.dump(Object,f)
+        self.left_widget.reset()
+        self.oleft_widget.reset()
         _translate = QtCore.QCoreApplication.translate
         self.label_9.setText(_translate("Form", "Image Name:" + self.file_list[self.num]))
         for comboBox_i in self.A_comboBox_list:
-            comboBox_i.setCurrentIndex(0)
+            comboBox_i.reset()
         for comboBox_i in self.O_comboBox_list:
-            comboBox_i.setCurrentIndex(0)
+            comboBox_i.reset()
+        self.QTextEdit.clear()
 
     def btn_add_val(self):
         my_key = self.comboBox_11.currentText()
         text = self.textEdit.toPlainText()
-        text_list = text.split(',')
+        text_list = text.split('，')
         _translate = QtCore.QCoreApplication.translate
         for i, key in enumerate(self.A_dict.keys()):
             if key == my_key:
                 lines = []
-                with open("Attribute") as f:
+                with open("Attribute", encoding='utf-8-sig') as f:
                     for line in f.readlines():
-                        temp = line[:-1].split(':')
+                        temp = line[:-1].split('：')
                         if temp[0] == my_key:
-                            lines.append(line[:-1] +',' +  text+'\n')
+                            lines.append(line[:-1] +'，' +  text+'\n')
                         else:
                             lines.append(line)
-                with open("Attribute",'w') as f:
+                with open("Attribute",'w',encoding='utf-8') as f:
                     s = ''.join(lines)
-                    f.write(s)
-                for j,txt in enumerate(text_list):
-                    self.A_comboBox_list[i].addItem("")
-                    self.A_comboBox_list[i].setItemText(self.A_comboBox_list[i].count()-1, _translate("Form", txt))
+                    f.write(str(s))
+                for txt in text_list:
+                    self.item = QListWidgetItem(txt, self.A_comboBox_list[i])  # 左侧选项的添加
+                    self.item.setSizeHint(QSize(15, 20))
+                    self.item.setTextAlignment(Qt.AlignCenter)  # 居中显示
+                self.A_dict[key] += text_list
         for i, key in enumerate(self.O_dict.keys()):
             if key == my_key:
                 lines = []
-                with open("Object") as f:
+                with open("Object",encoding="utf-8-sig") as f:
                     for line in f.readlines():
-                        temp = line[:-1].split(':')
+                        temp = line[:-1].split('：')
                         if temp[0] == my_key:
-                            lines.append(line[:-1] + ','+text+'\n')
+                            lines.append(line[:-1] + '，'+text+'\n')
                         else:
                             lines.append(line)
-                with open("Object",'w') as f:
+                with open("Object",'w',encoding='utf-8') as f:
                     s = ''.join(lines)
-                    f.write(s)
-                for j,txt in enumerate(text_list):
-                    self.O_comboBox_list[i].addItem("")
-                    self.O_comboBox_list[i].setItemText(self.O_comboBox_list[i].count()-1, _translate("Form", txt))
+                    f.write(str(s))
+                for txt in text_list:
+                    self.oitem = QListWidgetItem(txt,self.O_comboBox_list[i])  # 左侧选项的添加
+                    self.oitem.setSizeHint(QSize(15, 20))
+                    self.oitem.setTextAlignment(Qt.AlignCenter)  # 居中显示
+                self.O_dict[key] += text_list
         print([my_key,text])
 
         self.textEdit.setText("")
@@ -333,105 +342,92 @@ class Ui_Form(object):
             self.add_o = True
             self.add_a = False
     def btn_add_class(self):
-        _translate = QtCore.QCoreApplication.translate
         if self.add_a:
             self.radioButton.setChecked(True)
             my_key = self.lineEdit_2.text()
             my_val = self.lineEdit_3.text()
-            my_val = ['none'] + my_val.split(",")
+            my_val = my_val.split("，")
             if self.checkBox.isChecked():
                 if len(my_key) != 0:
-                    num_com = len(self.A_comboBox_list)
-                    i = num_com
-                    self.A_dict[my_key] = my_val
-                    locals()["A_comboBox" + str(i)] = QtWidgets.QComboBox(self.groupBox)
-                    if 60 * (i + 1) + 31 <= 391:
-                        locals()["A_comboBox" + str(i)].setGeometry(QtCore.QRect(10, 60 * (i + 1), 181, 31))
-                    else:
-                        locals()["A_comboBox" + str(i)].setGeometry(QtCore.QRect(10 + 191, 60 * (i + 1 - 6), 181, 31))
-                    locals()["A_comboBox" + str(i)].setEditable(False)
-                    locals()["A_comboBox" + str(i)].setObjectName("A_comboBox")
-                    for val in my_val:
-                        locals()["A_comboBox" + str(i)].addItem("")
-                    locals()["A_label" + str(i)] = QtWidgets.QLabel(self.groupBox)
-                    if 40 + 60 * i <= 391:
-                        locals()["A_label" + str(i)].setGeometry(QtCore.QRect(10, 40 + 60 * i, 200, 16))
-                    else:
-                        locals()["A_label" + str(i)].setGeometry(QtCore.QRect(10 + 191, 40 + 60 * (i - 6), 200, 16))
-                    font = QtGui.QFont()
-                    font.setPointSize(18)
-                    locals()["A_label" + str(i)].setFont(font)
-                    locals()["A_label" + str(i)].setObjectName("A_label")
-                    self.A_comboBox_list.append(locals()["A_comboBox" + str(i)])
-                    self.A_label_list.append(locals()["A_label" + str(i)])
+                    i = len(self.A_comboBox_list)
+                    self.item = QListWidgetItem(my_key, self.left_widget)  # 左侧选项的添加
+                    self.item.setSizeHint(QSize(15, 20))
+                    self.item.setTextAlignment(Qt.AlignCenter)  # 居中显示
 
-                    locals()["A_label" + str(i)] .setText(_translate("Form", my_key))
-                    for j, val in enumerate(my_val):
-                        locals()["A_comboBox" + str(i)].setItemText(j, _translate("Form", val))
-                    locals()["A_label" + str(i)].show()
-                    locals()["A_comboBox" + str(i)].show()
-                    self.comboBox_11.addItem("")
-                    self.comboBox_11.setItemText(len(self.A_comboBox_list)+len(self.O_comboBox_list)-1, _translate("Form", my_key))
+                    locals()['stack' + str(i)] = QListWidget()  # 左侧选项列表
+                    locals()['stack' + str(i)].setGeometry(QtCore.QRect(0, 0, 181, 360))
+                    locals()['stack' + str(i)].setStyleSheet(self.list_style)
+                    for att_v in my_val:
+                        self.ritem = QListWidgetItem(att_v, locals()['stack' + str(i)])  # 左侧选项的添加
+                        self.ritem.setSizeHint(QSize(15, 20))
+                        self.ritem.setTextAlignment(Qt.AlignCenter)  # 居中显示
+                    self.right_widget.addWidget(locals()['stack' + str(i)])
+                    self.A_comboBox_list.append(locals()["stack" + str(i)])
+                    locals()['stack' + str(i)].currentRowChanged.connect(self.row_change)
+                    self.A_dict[my_key] = my_val
                     lines = []
-                    with open("Attribute") as f:
+                    with open("Attribute", encoding="utf-8-sig") as f:
                         for line in f.readlines():
                             lines.append(line)
-                        lines.append("\n" + my_key + ":" + ",".join(my_val))
-                    with open("Attribute", 'w') as f:
+                        lines.append('\n' + my_key + "：" + "，".join(my_val))
+                    with open("Attribute", 'w', encoding="utf-8") as f:
                         s = ''.join(lines)
-                        f.write(s)
+                        f.write(str(s))
             else:
-                self.A_extra[my_key] = my_val[1:][0]
+                text = self.QTextEdit.toPlainText()
+                if len(text)!= 0:
+                    self.A_answer = eval(text.split('\n')[3])
+                    self.O_answer = eval(text.split('\n')[1])
+                else:
+                    self.A_answer = {}
+                    self.O_answer = {}
+                # print(A_an)
+                self.A_answer[my_key] = my_val[1:][0]
+                self.QTextEdit.setPlainText(
+                    str("对象:\n") + str(self.O_answer) + "\n" + str("属性:\n") + str(self.A_answer) + "\n")
         if self.add_o:
             self.add_o = False
             self.radioButton.setChecked(True)
             my_key = self.lineEdit_2.text()
             my_val = self.lineEdit_3.text()
-            my_val = ['none'] + my_val.split(",")
+            my_val = my_val.split("，")
             if self.checkBox.isChecked():
                 if len(my_key) != 0:
-                    num_com = len(self.O_comboBox_list)
-                    i = num_com
-                    self.O_dict[my_key] = my_val
-                    locals()["O_comboBox" + str(i)] = QtWidgets.QComboBox(self.groupBox_5)
-                    if 60 * (i + 1) + 31 < 400:
-                        locals()["O_comboBox" + str(i)].setGeometry(QtCore.QRect(10, 60 * (i + 1), 181, 31))
-                    else:
-                        locals()["O_comboBox" + str(i)].setGeometry(QtCore.QRect(10 + 191, 60 * (i + 1 - 6), 181, 31))
-                    locals()["O_comboBox" + str(i)].setEditable(False)
-                    locals()["O_comboBox" + str(i)].setObjectName("A_comboBox")
-                    for val in my_val:
-                        locals()["O_comboBox" + str(i)].addItem("")
-                    locals()["O_label" + str(i)] = QtWidgets.QLabel(self.groupBox_5)
-                    if 40 + 60 * i < 400:
-                        locals()["O_label" + str(i)].setGeometry(QtCore.QRect(10, 40 + 60 * i, 200, 16))
-                    else:
-                        locals()["O_label" + str(i)].setGeometry(QtCore.QRect(10 + 191, 40 + 60 * (i - 6), 200, 16))
-                    font = QtGui.QFont()
-                    font.setPointSize(18)
-                    locals()["O_label" + str(i)].setFont(font)
-                    locals()["O_label" + str(i)].setObjectName("O_label")
-                    self.O_comboBox_list.append(locals()["O_comboBox" + str(i)])
-                    self.O_label_list.append(locals()["O_label" + str(i)])
+                    i = len(self.O_comboBox_list)
+                    self.oitem = QListWidgetItem(my_key, self.oleft_widget)  # 左侧选项的添加
+                    self.oitem.setSizeHint(QSize(15, 20))
+                    self.oitem.setTextAlignment(Qt.AlignCenter)  # 居中显示
 
-                    locals()["O_label" + str(i)].setText(_translate("Form", my_key))
-                    for j, val in enumerate(my_val):
-                        locals()["O_comboBox" + str(i)].setItemText(j, _translate("Form", val))
-                    locals()["O_label" + str(i)].show()
-                    locals()["O_comboBox" + str(i)].show()
-                    self.comboBox_11.addItem("")
-                    self.comboBox_11.setItemText(len(self.A_comboBox_list) + len(self.O_comboBox_list)-1,
-                                                 _translate("Form", my_key))
+                    locals()['ostack' + str(i)] = QListWidget()  # 左侧选项列表
+                    locals()['ostack' + str(i)].setGeometry(QtCore.QRect(0, 0, 181, 360))
+                    locals()['ostack' + str(i)].setStyleSheet(self.olist_style)
+                    for obj_v in my_val:
+                        self.oritem = QListWidgetItem(obj_v, locals()['ostack' + str(i)])  # 左侧选项的添加
+                        self.oritem.setSizeHint(QSize(15, 20))
+                        self.oritem.setTextAlignment(Qt.AlignCenter)  # 居中显示
+                    self.oright_widget.addWidget(locals()['ostack' + str(i)])
+                    self.O_comboBox_list.append(locals()["ostack" + str(i)])
+                    locals()['ostack' + str(i)].currentRowChanged.connect(self.orow_change)
+                    self.O_dict[my_key] = my_val
                     lines = []
-                    with open("Object") as f:
+                    with open("Object",encoding="utf-8-sig") as f:
                         for line in f.readlines():
                             lines.append(line)
-                        lines.append('\n' + my_key + ":" + ",".join(my_val))
-                    with open("Object", 'w') as f:
+                        lines.append('\n' + my_key + "：" + "，".join(my_val))
+                    with open("Object", 'w',encoding="utf-8") as f:
                         s = ''.join(lines)
-                        f.write(s)
+                        f.write(str(s))
             else:
-                self.O_extra[my_key] = my_val[1:][0]
+                text = self.QTextEdit.toPlainText()
+                if len(text) != 0:
+                    self.A_answer = eval(text.split('\n')[3])
+                    self.O_answer = eval(text.split('\n')[1])
+                else:
+                    self.A_answer = {}
+                    self.O_answer = {}
+                self.O_answer[my_key] = my_val[1:][0]
+                self.QTextEdit.setPlainText(
+                    str("对象:\n") + str(self.O_answer) + "\n" + str("属性:\n") + str(self.A_answer) + "\n")
         self.radio_flag = False
 
 
@@ -441,7 +437,7 @@ class Ui_Form(object):
 
     def btn4_back(self):
         if self.num < 1:
-            print("Exit!!!")
+            sys.exit(0)
         self.num = self.num - 1
         with open("count","w") as f:
             f.write(str(self.num))
@@ -451,9 +447,11 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         self.label_9.setText(_translate("Form", "Image Name:" + self.file_list[self.num]))
         for comboBox_i in self.A_comboBox_list:
-            comboBox_i.setCurrentIndex(0)
+            comboBox_i.reset()
         for comboBox_i in self.O_comboBox_list:
-            comboBox_i.setCurrentIndex(0)
+            comboBox_i.reset()
+        self.QTextEdit.clear()
+
 
 
 
